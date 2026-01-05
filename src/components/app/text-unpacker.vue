ebfe39540f255daf90ec03dda6d6d43d2c8de66b
lone-DO <citizin@outlook.com>
2026-01-05 10:33:10 +0100

    patch: Migrate text unpacker to independent component
    Signed-off-by: lone-DO <citizin@outlook.com>

---
+++ src/components/app/text-unpacker.vue
@@ -0,0 +1,22 @@
+<script lang='ts' setup>
+import { defineProps, withDefaults } from 'vue';
+
+withDefaults(defineProps<{
+	text: string;
+	className?: string;
+	baseWeight?: number;
+}>(), {
+	baseWeight: 400,
+});
+</script>
+
+<template>
+	<span
+		v-for="(char, index) in text"
+		:key="index"
+		:class="className"
+		:style="{ 'font-variation-settings': `whgt ${baseWeight}` }"
+	>
+		{{ char === ' ' ? '\u00A0' : char }}
+	</span>
+</template>
