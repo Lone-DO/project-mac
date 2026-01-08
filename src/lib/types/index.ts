dfb37d5c80b0e995f4222732f194c3fb2a59d87b
lone-DO <citizin@outlook.com>
2026-01-08 12:39:04 +0100

    feat: App Docker Styling + GSAP animations
    Signed-off-by: lone-DO <citizin@outlook.com>

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,2 +1,3 @@
 export * from './tooltip.ts';
 export type DockApp = { id: string; label: string; imgSrc: string; canOpen: boolean };
+export type DockListeners = [HTMLElement, (keyof HTMLElementEventMap), (e: any) => any][];
