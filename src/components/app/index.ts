2ce3b9e7dd80192658765bbbc6bb4f2ca253517c
lone-DO <citizin@outlook.com>
2026-01-09 13:24:25 +0100

    refactor: Update indexes
    Signed-off-by: lone-DO <citizin@outlook.com>

---
+++ src/components/app/index.ts
@@ -0,0 +1,7 @@
+import AppDock from './dock.vue';
+import AppHeader from './header.vue';
+import AppRootTooltip from './tooltip-root.vue';
+import AppTooltip from './tooltip.vue';
+import AppWelcome from './welcome.vue';
+
+export { AppDock, AppHeader, AppRootTooltip, AppTooltip, AppWelcome };
